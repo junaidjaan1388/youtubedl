3c1cddbd95d32de0404d3678423553c5669f1a73
junaidjaan1388 <112433148+junaidjaan1388@users.noreply.github.com>
2022-10-02 18:11:39 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
 
 with ydl:
     result = ydl.extract_info(
-        'http://www.youtube.com/watch?v=BaW_jenozKc',
-        download=False # We just want to extract the info
+        'https://youtu.be/aTgPJQ9Dy7Q',
+        download=True # We just want to extract the info
     )
 
 if 'entries' in result:
